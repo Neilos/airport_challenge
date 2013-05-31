4174cd0e686c2f0f8a2c4d32d21ef44e86d6a1b7
Neil Atkinson <nosniktatkinson@hotmail.co.uk>
2013-05-31 12:21:29 +0100

    setup initial classes and spec tests in minitest

---
+++ test/plane_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/mock'
+require '../lib/plane'
+
+describe Plane do
+  
+end
---
+++ test/weather_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/mock'
+require '../lib/weather'
+
+describe Weather do
+  
+end
